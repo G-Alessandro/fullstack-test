32701bddb919193cc094490176341fed28317d98
Alessandro <galloalessandro77@gmail.com>
2025-12-05 11:30:42 +0100

    feat(routes): add POST /transactions route

--- Api/routes/transactions.js
+++ Api/routes/transactions.js
@@ -2,9 +2,12 @@ const express = require('express');
 const controller = require('../controllers/transactions');
 const { isAuth } = require('../middlewares/isAuth');
 const rbac = require('../middlewares/rbac');
+const { validator } = require('../middlewares/validator');
 
 const router = express.Router();
 
-router.route('/').get(isAuth, rbac('transactions', 'read'), controller.get);
-
+router
+  .route('/')
+  .get(isAuth, rbac('transactions', 'read'), controller.get)
+  .post(validator('createTransaction'), isAuth, rbac('transactions', 'create'), controller.create);
 module.exports = router;
