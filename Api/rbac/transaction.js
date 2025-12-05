d018c9e9835fc467a85b4d9dbd8885994d89138e
Alessandro <galloalessandro77@gmail.com>
2025-12-05 14:38:48 +0100

    feat(rbac): add transaction rbac

---
+++ Api/rbac/transaction.js
@@ -0,0 +1,26 @@
+const { intersection } = require('../helpers/utils');
+const Transaction = require('../models/transaction');
+
+const transactionRbac = async (caller, resourceId, { authorizedRoles = [] }) => {
+  const transaction = await Transaction.findById(resourceId);
+  if (!transaction) return null;
+
+  const { grants, company: userCompany = {}, roles: globalRoles } = caller;
+  const { roles: companyRoles = [] } = userCompany;
+  const roles = Array.from(new Set([...companyRoles, ...globalRoles]));
+
+  if (grants?.type === 'any' || roles.includes('superuser')) return transaction;
+  if (intersection(authorizedRoles, roles).length && transaction.userId.toString() === caller.id.toString()) {
+    return transaction;
+  }
+  return false;
+};
+
+module.exports.canGetTransaction = (caller, resourceId) =>
+  transactionRbac(caller, resourceId, { authorizedRoles: ['admin', 'user'] });
+
+module.exports.canUpdateTransaction = (caller, resourceId) =>
+  transactionRbac(caller, resourceId, { authorizedRoles: ['admin'] });
+
+module.exports.canDeleteTransaction = (caller, resourceId) =>
+  transactionRbac(caller, resourceId, { authorizedRoles: ['admin'] });
