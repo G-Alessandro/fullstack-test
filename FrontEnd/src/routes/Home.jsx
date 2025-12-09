7a31ee13166ab18675973b3bc4c41265772b7cc1
Alessandro <galloalessandro77@gmail.com>
2025-12-09 01:44:16 +0100

    refactor(home): replace ContentPanel with TransactionsList component

--- FrontEnd/src/routes/Home.jsx
+++ FrontEnd/src/routes/Home.jsx
@@ -1,23 +1,4 @@
-import { useState } from 'react';
-import { Result } from 'antd';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
+import TransactionsList from '../components/core/transaction/TransactionsList';
 
-import ContentPanel from '../components/core/layout/ContentPanel';
-
-const Home = () => {
-  const [loading] = useState(false);
-
-  return (
-    <ContentPanel title="Fullstack Test" loading={loading}>
-      <Result
-        icon={<FontAwesomeIcon icon={faBook} size="4x" className="text-primary" />}
-        title="Expense and Income Diary"
-        subTitle="Create an application to track daily expenses and incomes. Users should be able
-        to add, read, update, and delete expense and income entries."
-      />
-    </ContentPanel>
-  );
-};
-
+const Home = () => <TransactionsList />;
 export default Home;
